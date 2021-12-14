d538a5a9b8888693a63121e0fa9a78a5d384340a
sanad <sanad@CHINETSU>
2021-12-14 13:44:18 +0800

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Kab2_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.cleanButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите ряд чисел:";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(161, 9);
+            this.textBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(285, 27);
+            this.textBox.TabIndex = 1;
+            this.textBox.TextChanged += new System.EventHandler(this.Input);
+            this.textBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CheckEnter);
+            // 
+            // cleanButton
+            // 
+            this.cleanButton.Location = new System.Drawing.Point(302, 48);
+            this.cleanButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cleanButton.Name = "cleanButton";
+            this.cleanButton.Size = new System.Drawing.Size(136, 52);
+            this.cleanButton.TabIndex = 2;
+            this.cleanButton.Text = "Очистка поля";
+            this.cleanButton.UseVisualStyleBackColor = true;
+            this.cleanButton.Click += new System.EventHandler(this.cleanButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 113);
+            this.Controls.Add(this.cleanButton);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button cleanButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kab2_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Input(object sender, EventArgs e)
+        {
+            if (textBox.Text.Length > 0)
+            {
+                char symbol = textBox.Text[^1];
+                if (!(symbol >= '0' && symbol <= '9') && !(symbol == ' '))
+                {
+                    MessageBox.Show("Ошибка ввода! Вводите только цифры или пробел");
+                    textBox.Text = textBox.Text[0..^1];
+                }
+            }
+        }
+
+        private void CheckEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                textBox.Text += " ";
+                int n = 0;
+                int x = 1;
+                int number = 0;
+                int previousNumber = 0;
+                for (int i = 0; i < textBox.Text.Length; i++)
+                {
+                    if (textBox.Text[i] >= '0' && textBox.Text[i] <= '9')
+                    {
+                        number = number * 10 + textBox.Text[i];
+                    }
+                    else
+                    {
+                        if (number == previousNumber && n < 1)
+                        {
+                            n = x;
+                        }
+                        x++;
+                        previousNumber = number;
+                        number = 0;
+                    }
+
+                }
+                if (n == 0)
+                {
+                    MessageBox.Show("Рядом стоящих пар одинаковых чисел нет");
+                }
+                else
+                {
+                    MessageBox.Show("Одинаковые числа под номерами " + (n - 1) + " и " + n);
+                }
+            }
+        }
+
+        private void cleanButton_Click(object sender, EventArgs e)
+        {
+            textBox.Text = "";
+            textBox.Focus();
+        }
+    }
+}
+
