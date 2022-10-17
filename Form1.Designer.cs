eac40e579a6e08f0aa9ba5fcc96ac0cdc2c3682d
даниил дикальчук <sana.dana.81@hotmail.com>
2022-10-17 09:55:32 +0800

    Готово

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace Kab2_2
             this.label1 = new System.Windows.Forms.Label();
             this.textBox = new System.Windows.Forms.TextBox();
             this.cleanButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -64,11 +65,22 @@ namespace Kab2_2
             this.cleanButton.UseVisualStyleBackColor = true;
             this.cleanButton.Click += new System.EventHandler(this.cleanButton_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Найти пары";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(450, 113);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.cleanButton);
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.label1);
@@ -85,6 +97,7 @@ namespace Kab2_2
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox;
         private System.Windows.Forms.Button cleanButton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,8 @@ namespace Kab2_2
         public Form1()
         {
             InitializeComponent();
+            textBox.Text = Properties.Settings.Default.sequence;
+            textBox.SelectionStart = textBox.Text.Length;
         }
         private void Input(object sender, EventArgs e)
         {
@@ -25,6 +27,12 @@ namespace Kab2_2
                 {
                     MessageBox.Show("Ошибка ввода! Вводите только цифры или пробел");
                     textBox.Text = textBox.Text[0..^1];
+                    textBox.SelectionStart = textBox.Text.Length;
+                }
+                if (symbol == ' ' && textBox.Text[textBox.Text.Length - 2] == ' ')
+                {
+                    textBox.Text = textBox.Text.TrimEnd(' ');
+                    textBox.Text += symbol;
                 }
             }
         }
@@ -38,6 +46,8 @@ namespace Kab2_2
                 int x = 1;
                 int number = 0;
                 int previousNumber = 0;
+                Properties.Settings.Default.sequence = textBox.Text;
+                Properties.Settings.Default.Save();
                 for (int i = 0; i < textBox.Text.Length; i++)
                 {
                     if (textBox.Text[i] >= '0' && textBox.Text[i] <= '9')
@@ -71,6 +81,45 @@ namespace Kab2_2
         {
             textBox.Text = "";
             textBox.Focus();
+            Properties.Settings.Default.sequence = textBox.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox.Text += " ";
+            int n = 0;
+            int x = 1;
+            int number = 0;
+            int previousNumber = 0;
+            Properties.Settings.Default.sequence = textBox.Text;
+            Properties.Settings.Default.Save();
+            for (int i = 0; i < textBox.Text.Length; i++)
+            {
+                if (textBox.Text[i] >= '0' && textBox.Text[i] <= '9')
+                {
+                    number = number * 10 + textBox.Text[i];
+                }
+                else
+                {
+                    if (number == previousNumber && n < 1)
+                    {
+                        n = x;
+                    }
+                    x++;
+                    previousNumber = number;
+                    number = 0;
+                }
+
+            }
+            if (n == 0)
+            {
+                MessageBox.Show("Рядом стоящих пар одинаковых чисел нет");
+            }
+            else
+            {
+                MessageBox.Show("Одинаковые числа под номерами " + (n - 1) + " и " + n);
+            }
         }
     }
 }
